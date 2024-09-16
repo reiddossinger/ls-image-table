d65a7d79c8f341e7e13b365db37319ef9461dee0
reiddossinger <reiddossinger@gmail.com>
2024-09-16 17:04:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// index.js
 const dscc = require('@google/dscc');
-const local = require('./localMessage.js');  // For local testing only
+const local = require('./localMessage.js'); // For local testing only
 
 const drawViz = (data) => {
   const imgContainer = document.createElement('div');
   imgContainer.style.textAlign = "center";
 
   const img = document.createElement('img');
-  img.src = data.tables.DEFAULT[0].imageUrl;  // Assuming 'imageUrl' is your field name
+  img.src = data.tables.DEFAULT[0].imageUrl; // Assuming 'imageUrl' is your field name
   img.alt = 'Image';
   img.style.maxWidth = '100%';
   img.style.height = 'auto';
